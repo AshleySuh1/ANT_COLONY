1dfab7a27aa9c333b12c7c901c1caf9137cb0dc6
AshleySuh1 <118965395+AshleySuh1@users.noreply.github.com>
2022-12-04 04:53:02 -0800

    Update Ant_Simulation.m
    EDITS
    lines 11-24: I think this is a little bit neater? You can just input name of file you need. Also lets the TA input other maps we might not have.
    
    lines 29&30: I think there should be more random angles to explore with sigma2 bc thats the ants without pheromones in their area. They need to explore more to find pheromones.
    
    line 39: One of the requirements was to make the ant starting angle go around the entire colony in a circle, not the same angle. I'm not sure how to put the structures into an array better than this ;-;
    
    Lines 48 and below: Ants with food follow blue, ants without food follow red. The current code makes it so the ants follow any and all pheromones. I tried to fix this by making two sets of array for each type of pheromone....
    
    Line 66: So ant without food only looks at pheromone1 array which should only contain red pheromones, and vice versa
    
    Line 77 & 85: Just corresponding to the T/F changes i made in CheckColonyProximity and CheckFoodProximity
    
    Lines 97-110: Since there's two different sets, we don't need to check type anymore. I think at this point the second column for the pheromones and concentrations can be deleted? idk
    
    Line 130: I just kinda repeat the while loop to go through red and blue separately
    Line 190: Also just repeat the plotting thing to do red and blue separately
    
    REQUIREMENTS
    part of the requirement: There is supposed to be a high amount of blue pheromones at the colony and a high amount of red pheromones on each food. I attempted to add this in the pheromones and concentration arrays in the beginning indices, but it did not work for me ;;;;;; also, blue pheromone at colony is permanent but red pheromones at food should completely (w/out decay) disappear when the ant takes it right? ;;; can u maybe try to figure this out, I am not sure where to head from here. 
    
    EFFICIENCY
    -There was a "drawnow" command that can just change updates, so we don't have to use the clf function to remake the whole graph each frame. My brain kinda hurts rn and I'd like to figure out the pheromones on the colony and foods b4 attempting to clean anything tho...
    -I think efficiency also increases when you make arrays with a set length than just []? For the arrays of pheromone and concentrations, we can predict max size. For pheromone1 it's going to be (1/deltaB) + 1 if rem(1,deltaB)>0. To be honest, I don't this will help much though.
    -I'll look into this more later

--- Ant_Simulation.m
+++ Ant_Simulation.m
@@ -5,70 +5,52 @@ close all;
 rng(1111); % random seed
 
 % loaded data from .mat file is a struct 
-mapName = "map2"; % choosing the map to access the parameters
 % fixed parameters (only load the map that is needed)
 %=========================Load Data==========================
-switch mapName
-	case "map1"	
-		% load the map
-		map1 = load('map/map1.mat'); 
-		% assign the parameters
-		time = map1.T; 
-		colonyPos = map1.colony_pos; 
-		colonyProx = map1.colony_proximity_threshold; 
-		foodProx = map1.food_proximity_threshold;
-		foodSource = map1.food_sources; 
-		mapCoord = map1.map_coordinates; 
-		nAnts = map1.n_ants; 
-		walls = [0 0 0 0]; % initialize wall as empty
-	case "map2" 
-		% load the map
-		map2 = load('map/map2.mat'); 
-		% assign the parameters
-		time = map2.T; 
-		colonyPos = map2.colony_pos; 
-		colonyProx = map2.colony_proximity_threshold; 
-		foodProx = map2.food_proximity_threshold;
-		foodSource = map2.food_sources; 
-		mapCoord = map2.map_coordinates; 
-		nAnts = map2.n_ants; 
-		walls = [0 0 0 0]; % initialize wall as empty
-	case "mapWall"
-		% load the map
-		mapWall = load("map/map3_ExtraCredit.mat"); 
-		% assign the parameters
-		time = mapWall.T; 
-		colonyPos = mapWall.colony_pos; 
-		colonyProx = mapWall.colony_proximity_threshold; 
-		foodProx = mapWall.food_proximity_threshold;
-		foodSource = mapWall.food_sources; 
-		mapCoord = mapWall.map_coordinates; 
-		nAnts = mapWall.n_ants; 
-		walls = mapWall.walls;
-	otherwise
-		error("Name of the map is wrond"); 
+% load the map
+map = load('map2.mat'); 
+% assign the parameters
+time = map.T; 
+colonyPos = map.colony_pos; 
+colonyProx = map.colony_proximity_threshold; 
+foodProx = map.food_proximity_threshold;
+foodSource = map.food_sources; 
+mapCoord = map.map_coordinates; 
+nAnts = 5; %map.n_ants; 
+if length(fieldnames(map)) > 7
+    walls = map.walls; % initialize walls
+else
+    walls = [0 0 0 0]; % initialize wall as empty
 end
 
 % customizable parameters (to tune parameters)
 speed = 10; % speed of the ants (step size of ants in each time stamp)
 rSmell = 5; % size of the radius (to smell pheromones)
-sigma1 = 20; % angle update coefficient (with food in r_smell) 
-sigma2 = 2; % angle update coefficient (without food in r_smell) 
+sigma1 = 2; % angle update coefficient (with pheromones in r_smell) 
+sigma2 = 20; % angle update coefficient (without pheromones in r_smell) 
 deltaR = 0.1; % linear decay for red pheromone
 deltaB = 0.1; % linear decay for blue pheromon
 
 % initialize the ants
-% create the ant struct
-ant = struct; 
-ant.x = colonyPos(1) ; % ant's x position
-ant.y = colonyPos(2); % ant's y position
-ant.angle = 0; % ant's current angle
-ant.foodStatus = false; % status indicates whethers ants is carrying food
-ants = repmat(ant, 1, nAnts);% create the ants with array of struct 
+for n = 1:nAnts
+    ant = struct; 
+    ant.x = colonyPos(1) ; % ant's x position
+    ant.y = colonyPos(2); % ant's y position
+    ant.angle = 2*pi/nAnts*n; % ant's current angle
+    ant.foodStatus = false; % status indicates whethers ants is carrying food
+    if n == 1
+        ants = ant;
+    else
+        ants(n) = ant;
+    end
+end
 
 % initialize pheromones
-pheromones = []; % x_pos, y_pos
-concentration = []; % concentration, indicator 1:blue, 2: red
+pheromones1 = []; % x_pos, y_pos For Blue
+concentration1 = []; % concentration, indicator 1:blue, 2: red
+
+pheromones2 = []; % For red 
+concentration2 = []; 
 
 % colony food counter variable
 colonyFood = 0; % counter for the food inside the colony
@@ -82,13 +64,17 @@ for tCurrent = 1:time % iterate over timestamps (i.e., for each timestamp...)
 	%=========================Update Parameters========================
 	for i = 1:length(ants) % iterate over ants (i.e., for each ant...) 
 		% compute the new angle	
-		[newAngle] = ComputeNewAngle(ants(i).x, ants(i).y, ants(i).angle, pheromones, concentration, rSmell, sigma1, sigma2); 
+        	if ants(i).foodStatus == false
+			[newAngle] = ComputeNewAngle(ants(i).x, ants(i).y, ants(i).angle, pheromones2, concentration2, rSmell, sigma1, sigma2); 
+       		else
+                	[newAngle] = ComputeNewAngle(ants(i).x, ants(i).y, ants(i).angle, pheromones1, concentration1, rSmell, sigma1, sigma2);
+        	end 
 		% check movement validity + update ant loction and angle
 		[ants(i).x, ants(i).y, ants(i).angle] = MovementValidationExecution(ants(i).x, ants(i).y, newAngle, speed, mapCoord, walls);	
 		% if ant is not carrying food, check the food proximity and grab food if it's close to a source.
 		if ants(i).foodStatus == false
 			[foodSource, indicator] = CheckFoodProximity(ants(i).x, ants(i).y, foodSource, foodProx);
-			if indicator == true
+			if indicator == 1
 				ants(i).foodStatus = true; 
 				ants(i).angle = rem(ants(i).angle + pi, 2*pi); 
 			end 
@@ -96,7 +82,7 @@ for tCurrent = 1:time % iterate over timestamps (i.e., for each timestamp...)
 		% else, check the colony proximity and drop the food if it's close.
 		[indicator] = CheckColonyProximity(ants(i).x, ants(i).y, colonyPos, colonyProx); 
 		% check whether it should drop the food
-		if indicator == true
+		if indicator == 1
 			if ants(i).foodStatus == true
 				% drop the food 
 				ants(i).foodStatus = false; 
@@ -108,48 +94,73 @@ for tCurrent = 1:time % iterate over timestamps (i.e., for each timestamp...)
 	end % end iterate over ants	
 	%========================Update Pheromone===========================
 	% decay each diff pheromones and concentration 
-	if isempty(concentration) == false
-		[rows, cols] = size(concentration); 
+	% for the blue pheromones
+	if isempty(concentration1) == false	
+        	[rows, cols] = size(concentration1); 
 		for conInd = 1:rows
-			if concentration(conInd,2) == 1 % if it's a blue pheromone
-				concentration(conInd,1) = concentration(conInd,1) - deltaB; 
-			elseif concentration(conInd, 2) == 2 % if it's a red pheromone
-				concentration(conInd,1) = concentration(conInd,1) - deltaR; 
-			end
+            		concentration1(conInd,1) = concentration1(conInd,1) - deltaB;
+        	end
+    	end
+        % for the red pheromones
+    	if isempty(concentration2) == false
+        	[rows2, cols2] = size(concentration2);
+        	for conInd = 1:rows2
+            		concentration2(conInd,1) = concentration2(conInd,1) - deltaR; 
+       		end
+    	end
+
+	% remove the below zero concentration pheromone (blue)
+	curInd = 1; 
+	while true
+		% stopping point
+		[curRows, curCols] = size(concentration1); 
+		if curInd > curRows || curRows == 0
+			break; 
 		end
-		% remove the below zero concentration pheromone 
-		curInd = 1; 
-		while true
-			% stopping point
-			[curRows, curCols] = size(concentration); 
-			if curInd > curRows || curRows == 0
-				break; 
-			end
-			% delete the below zero pheromone
-			if concentration(curInd,1) <= 0
-				concentration(curInd,:) = []; 
-				pheromones(curInd,:) = []; 
-			else 
-				% keep looking for the next index
-				curInd = curInd + 1; 
-			end
+		% delete the below zero pheromone
+		if concentration1(curInd,1) <= 0
+			concentration1(curInd,:) = []; 
+			pheromones1(curInd,:) = []; 
+		else 
+			% keep looking for the next index
+			curInd = curInd + 1; 
 		end
 	end
+	
+	% remove the below zero concentration pheromone (red)
+    	curInd = 1; 
+    	while true
+		% stopping point
+		[curRows, curCols] = size(concentration2); 
+		if curInd > curRows || curRows == 0
+			break; 
+		end
+		% delete the below zero pheromone
+		if concentration2(curInd,1) <= 0
+			concentration2(curInd,:) = []; 
+			pheromones2(curInd,:) = []; 
+		else 
+			% keep looking for the next index
+			curInd = curInd + 1; 
+		end
+	end
+	
 	% udpate pheromones and concentration with ants' position
 	for antInd = 1:length(ants)
-		[rows, cols] = size(pheromones); 
+		[rows, cols] = size(pheromones2); 
+       		[rows1, cols1] = size(pheromones1);
 		if ants(antInd).foodStatus == true
 			% drop red pheromone if ant carries food
 			curX = ants(antInd).x;
 			curY = ants(antInd).y; 
-		 	pheromones(rows+1,:) = [curX curY]; 
-			concentration(rows+1,:) = [1 2];
+		 	pheromones2(rows+1,:) = [curX curY]; 
+			concentration2(rows+1,:) = [1 2];
 		else
 			% if it's empty, then drop blue pheromone
 			curX = ants(antInd).x;
 			curY = ants(antInd).y;
-		 	pheromones(rows+1,:) = [curX curY]; 
-			concentration(rows+1,:) = [1 1];
+		 	pheromones1(rows+1,:) = [curX curY]; 
+			concentration1(rows+1,:) = [1 1];
 		end	
 	end
 	%=========================Start Plotting============================
@@ -167,19 +178,26 @@ for tCurrent = 1:time % iterate over timestamps (i.e., for each timestamp...)
 	if isempty(pheromones) == false
 		[rows, cols] = size(pheromones); 	
 		for phInd = 1:rows
-			curX = pheromones(phInd,1); 
-			curY = pheromones(phInd,2); 
+			curX = pheromones1(phInd,1); 
+			curY = pheromones1(phInd,2); 
 			con = concentration(phInd,1); 
 			if con < 1e-1
 				con = 0; 
 			end 
-			type = concentration(phInd,2); 
-			if type == 1 % blue pheromone
-				plot(curX, curY, '.', 'Color', [1-con, 1-con, 1], 'MarkerSize', 10); 
-			else 
-				plot(curX, curY, '.', 'Color', [1, 1-con, 1-con], 'MarkerSize', 10); 
-			end
+			plot(curX, curY, '.', 'Color', [1-con, 1-con, 1], 'MarkerSize', 10); 
 		end	
+	end
+	if isempty(pheromones2) == false
+        [rows2, cols2] = size(pheromones2); 	
+		for phInd = 1:rows2
+			curX = pheromones2(phInd,1); 
+			curY = pheromones2(phInd,2); 
+			con = concentration2(phInd,1); 
+			if con < 1e-1
+				con = 0; 
+			end 
+			plot(curX, curY, '.', 'Color', [1, 1-con, 1-con], 'MarkerSize', 5); 				 			
+        	end	
 	end
 	%***************Plot the Wall*****************
 	[nWall, column] = size(walls); 	
